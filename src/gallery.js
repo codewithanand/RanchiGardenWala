21dd4ce257f6ed78f7e1fb9701fec4a3a0b740f7
chandan kumar <128124398+chyawanprashwala@users.noreply.github.com>
2023-04-09 16:58:03 +0530

    Update gallery.js

--- src/gallery.js
+++ src/gallery.js
@@ -8,11 +8,29 @@ export default {
                 "description": ""
             },
             {
-                "id": 2,
-                "url": "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg",
-                "title": "Beautiful Plant",
+                "id": 9,
+                "url": "https://de.m.wikipedia.org/wiki/Kristall_(Rose)",
+                "title": "Rose (Rosa 'Kristall')",
                 "description": "lorem ipsum"
             },
+            {
+                "id": 10,
+                "url": "https://en.m.wikipedia.org/wiki/Solenostemon",
+                "title": "Solenostemon",
+                "description": "Solenostemon is a former genus of flowering plants in the family Lamiaceae. It has been included in the genus Plectranthus, but is now included in an expanded Coleus.[1][2] They are native to tropical Africa, Asia and Australia. Some species formerly placed in this genus are cultivated for their highly variegated leaves."
+            },
+            {
+                "id": 11,
+                "url": "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg",
+                "title": "Shyama Tulsi",
+                "description": "Is there any difference between Shyama Tulsi and Rama Tulsi?
+                                The major difference between Rama and Shyama Tulsi is the leaves of the plants. The leaves of Shyama Tulsi are purplish in colour, the same as the skin colour of Lord Krishna and the leaves of Rama Tulsi are green in colour and the plant derives its name from Lord Rama.
+                                https://mygreenstore.co.in/products/shyama-tulsi"
+            },
+          
+
+           
+
         ]
     }
 }
